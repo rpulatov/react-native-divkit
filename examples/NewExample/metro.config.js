df6effafaaa2735e965d2578476f2fd68e899760
Ruslan Pulatov <ufan4ik@mail.ru>
2026-02-07 02:26:32 +0700

    chore: update dependencies for React and React Native
    - Updated React to version 19.2.4
    - Updated React Native to version 0.83.1
    - Updated react-test-renderer to version 19.2.0
    - Updated TypeScript to version 5.8.3

--- examples/NewExample/metro.config.js
+++ examples/NewExample/metro.config.js
@@ -1,7 +1,10 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const path = require('path');
+const { default: exclusionList } = require('metro-config/private/defaults/exclusionList');
 
 const root = path.resolve(__dirname, '../..');
+const rootNodeModules = path.resolve(root, 'node_modules');
+const exampleNodeModules = path.resolve(__dirname, 'node_modules');
 
 /**
  * Metro configuration
@@ -12,7 +15,15 @@ const root = path.resolve(__dirname, '../..');
 const config = {
   watchFolders: [root],
   resolver: {
-    nodeModulesPaths: [path.resolve(__dirname, 'node_modules')],
+    nodeModulesPaths: [exampleNodeModules],
+    blockList: exclusionList([
+      new RegExp(`${rootNodeModules.replace(/[/\\]/g, '[/\\\\]')}[/\\\\]react[/\\\\].*`),
+      new RegExp(`${rootNodeModules.replace(/[/\\]/g, '[/\\\\]')}[/\\\\]react-native[/\\\\].*`),
+    ]),
+    extraNodeModules: {
+      react: path.resolve(exampleNodeModules, 'react'),
+      'react-native': path.resolve(exampleNodeModules, 'react-native'),
+    },
   },
 };
 
