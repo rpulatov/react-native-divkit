3b82ce27e247c393ab8e4edda9afc02727491be1
Ruslan Pulatov <ufan4ik@mail.ru>
2026-02-07 02:00:21 +0700

    Revert "fix: Починить тесты в примере"
    This reverts commit bd4d91dbd6d374c3addd7faef4aca7ac07dced41.

--- examples/NewExample/jest.config.js
+++ examples/NewExample/jest.config.js
@@ -1,13 +1,3 @@
-const path = require('path');
-
 module.exports = {
   preset: 'react-native',
-  moduleNameMapper: {
-    '^react$': path.resolve(__dirname, 'node_modules/react'),
-    '^react/(.*)$': path.resolve(__dirname, 'node_modules/react/$1'),
-    '^react-native$': path.resolve(__dirname, 'node_modules/react-native'),
-    '^react-native/(.*)$': path.resolve(__dirname, 'node_modules/react-native/$1'),
-    '^react-native-svg$': path.resolve(__dirname, 'node_modules/react-native-svg'),
-    '^@babel/runtime/(.*)': path.resolve(__dirname, 'node_modules/@babel/runtime/$1'),
-  },
 };
